098d1221646f4c6f3b337bdf413653c952f37c2b
Lira Guedes <liracrg@gmail.com>
2023-06-20 16:53:37 -0300

    Requisito 6 e 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
-      <div>Table</div>
+      <div>
+        <div>Table</div>
+        <header>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </header>
+        <tbody>
+          {
+            expenses.map((elem) => (
+              <tr key={ elem.id }>
+                <td>{ elem.description }</td>
+                <td>{ elem.tag }</td>
+                <td>{ elem.method }</td>
+                <td>{ elem.value }</td>
+                <td>{ elem.exchangeRates[elem.currency].name }</td>
+                <td>{ elem.exchangeRates[elem.currency].ask }</td>
+                <td>{ elem.value * elem.exchangeRates[elem.currency].ask }</td>
+                <td>Real</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: propTypes.arrayOf(propTypes.shape()).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <WalletForm />
+        <Table />
       </div>
     );
   }
